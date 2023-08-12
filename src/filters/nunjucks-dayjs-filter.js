52a4a105742638ada099c61416df41659012458b
Moggach <acunnane13@gmail.com>
2023-08-12 19:50:18 +0100

    run prettier

--- src/filters/nunjucks-dayjs-filter.js
+++ src/filters/nunjucks-dayjs-filter.js
@@ -1,9 +1,9 @@
-const dayjs = require('dayjs')
+const dayjs = require("dayjs");
 
-const defaultFormat = 'DD MMM YYYY'
+const defaultFormat = "DD MMM YYYY";
 
 function dayjsFilter(date, format = defaultFormat) {
-  return dayjs(date).format(format)
+  return dayjs(date).format(format);
 }
 
-module.exports = dayjsFilter
+module.exports = dayjsFilter;
