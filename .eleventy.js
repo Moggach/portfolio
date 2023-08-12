86653eb59a41318f86e82b0b123ee2c79bb494c6
Moggach <acunnane13@gmail.com>
2023-08-12 19:22:14 +0100

    install prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,25 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/images/");
-    const markdownIt = require('markdown-it');
-    const markdownItAttrs = require('markdown-it-attrs');
+  eleventyConfig.addPassthroughCopy("./src/images/");
+  const markdownIt = require("markdown-it");
+  const markdownItAttrs = require("markdown-it-attrs");
 
-
-    const markdownItOptions = {
-      html: true,
-      breaks: true,
-      linkify: true
-    }
-    
-const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
-eleventyConfig.setLibrary('md', markdownLib)
-eleventyConfig.addNunjucksFilter('date', require('./src/filters/nunjucks-dayjs-filter'))
-
-  
-    return {
-      dir: {
-        input: "src",
-        output: "public"
-      }
-    };
+  const markdownItOptions = {
+    html: true,
+    breaks: true,
+    linkify: true,
   };
+
+  const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs);
+  eleventyConfig.setLibrary("md", markdownLib);
+  eleventyConfig.addNunjucksFilter(
+    "date",
+    require("./src/filters/nunjucks-dayjs-filter"),
+  );
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
