4ffac7053d1be09759a5e5f1595d45a2ef9a2b4f
Moggach <acunnane13@gmail.com>
2023-08-12 17:54:52 +0100

    add sass and html tag support

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/css/");
-    eleventyConfig.addWatchTarget("./src/css/");
     eleventyConfig.addPassthroughCopy("./src/images/");
+    const markdownIt = require('markdown-it');
+    const markdownItAttrs = require('markdown-it-attrs');
 
+    const markdownItOptions = {
+      html: true,
+      breaks: true,
+      linkify: true
+    }
+    
+const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
+eleventyConfig.setLibrary('md', markdownLib)
   
     return {
       dir: {
