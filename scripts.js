142b36f440b9e8c28425f73f768ae7b7ced11a6d
Moggach <acunnane13@gmail.com>
2023-09-10 17:12:05 +0100

    test netlify form integration

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("myForm");
+    const thankYouMessage = document.getElementById("thankYouMessage");
+  
+    form.addEventListener("submit", async function (event) {
+      event.preventDefault();
+  
+      // Perform any form submission logic here, e.g., sending data to a server.
+      
+      // Once the form is submitted successfully, display the thank you message.
+      thankYouMessage.style.display = "block";
+  
+      // You can also reset the form if needed.
+      form.reset();
+    });
+  });
+  
