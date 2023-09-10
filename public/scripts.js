e1dee3127017d0b1dfe103312a01e30d0bd21ee4
Moggach <acunnane13@gmail.com>
2023-09-10 17:22:48 +0100

    add it back in

--- public/scripts.js
+++ public/scripts.js
@@ -1,17 +1,17 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const form = document.getElementById("contactForm");
-//     const thankYouMessage = document.getElementById("thankYouMessage");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contactForm");
+    const thankYouMessage = document.getElementById("thankYouMessage");
   
-//     form.addEventListener("submit", async function (event) {
-//       event.preventDefault();
+    form.addEventListener("submit", async function (event) {
+      event.preventDefault();
   
-//       // Perform any form submission logic here, e.g., sending data to a server.
+      // Perform any form submission logic here, e.g., sending data to a server.
       
-//       // Once the form is submitted successfully, display the thank you message.
-//       thankYouMessage.style.display = "block";
+      // Once the form is submitted successfully, display the thank you message.
+      thankYouMessage.style.display = "block";
   
-//       // You can also reset the form if needed.
-//       form.reset();
-//     });
-//   });
+      // You can also reset the form if needed.
+      form.reset();
+    });
+  });
   
