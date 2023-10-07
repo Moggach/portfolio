8fa4507cb420fbe6385e5d7cae4cc4659b473ae9
Moggach <acunnane13@gmail.com>
2023-10-07 20:37:03 +0100

    add word limit filter file

---
+++ src/filters/word-limit-filter.js
@@ -0,0 +1,7 @@
+module.exports = function (content, limit) {
+    const words = content.split(' ');
+    if (words.length > limit) {
+      return words.slice(0, limit).join(' ') + '...';
+    }
+    return content;
+  };
