da3690b22350c1922bcbff3855ad35a643f92a60
Moggach <acunnane13@gmail.com>
2024-02-01 18:32:53 +0000

    fix slider reverting to light mode side on page refresh

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,54 +9,56 @@ document.querySelectorAll('nav li a').forEach(function (link) {
     link.classList.add('active');
   }
 });
-  const body = document.body;
+const body = document.body;
 
-  // Check if there's a saved mode in localStorage
-  const savedMode = localStorage.getItem("darkMode");
+// Check if there's a saved mode in localStorage
+const savedMode = localStorage.getItem("darkMode");
 
-  // Set the initial state based on the saved mode (or default to light mode)
-  if (savedMode === "dark") {
-    body.classList.add("dark-mode");
+// Set the initial state based on the saved mode (or default to light mode)
+if (savedMode === "dark") {
+  body.classList.add("dark-mode");
+} else {
+  body.classList.remove("dark-mode");
+}
+
+const sliderContainer = document.querySelector('.slider-container');
+const sliderIcon = document.querySelector('.slider-icon');
+
+// Calculate the maximum translation for the sliderIcon
+const containerWidth = sliderContainer.clientWidth;
+const iconWidth = sliderIcon.clientWidth;
+const maxX = containerWidth - iconWidth;
+
+// Set an initial position for the sliderIcon based on the savedMode
+sliderIcon.style.transform = savedMode === "dark" ? `translateX(${maxX}px)` : 'translateX(0)';
+
+sliderContainer.addEventListener('click', function () {
+  const iconPosition = sliderIcon.style.transform;
+
+  if (iconPosition === 'translateX(0px)') {
+    sliderIcon.style.transform = `translateX(${maxX}px)`;
   } else {
-    body.classList.remove("dark-mode");
+    sliderIcon.style.transform = 'translateX(0)';
   }
+});
 
-  const sliderContainer = document.querySelector('.slider-container');
-  const sliderIcon = document.querySelector('.slider-icon');
+let isUserInteracted = false; // Flag to track user interaction
 
-  // Set an initial position for the sliderIcon
-  sliderIcon.style.transform = 'translateX(0)';
-
-  sliderContainer.addEventListener('click', function () {
-    const containerWidth = sliderContainer.clientWidth;
-    const iconWidth = sliderIcon.clientWidth;
-    const maxX = containerWidth - iconWidth;
-    const iconPosition = sliderIcon.style.transform;
-
-    if (iconPosition === 'translateX(0px)') {
-      sliderIcon.style.transform = `translateX(${maxX}px)`;
+// Add an event listener for the transitionend event
+sliderIcon.addEventListener('transitionend', function () {
+  if (isUserInteracted) {
+    if (body.classList.contains("dark-mode")) {
+      body.classList.remove("dark-mode");
+      localStorage.setItem("darkMode", "light");
     } else {
-      sliderIcon.style.transform = 'translateX(0)';
+      body.classList.add("dark-mode");
+      localStorage.setItem("darkMode", "dark");
     }
-  });
+  }
+});
 
-  let isUserInteracted = false; // Flag to track user interaction
-
-  // Add an event listener for the transitionend event
-  sliderIcon.addEventListener('transitionend', function () {
-    if (isUserInteracted) {
-      if (body.classList.contains("dark-mode")) {
-        body.classList.remove("dark-mode");
-        localStorage.setItem("darkMode", "light");
-      } else {
-        body.classList.add("dark-mode");
-        localStorage.setItem("darkMode", "dark");
-      }
-    }
-  });
-
-  // Add a click event listener to the sliderContainer to set the flag
-  sliderContainer.addEventListener('click', function () {
-    isUserInteracted = true;
-  });
+// Add a click event listener to the sliderContainer to set the flag
+sliderContainer.addEventListener('click', function () {
+  isUserInteracted = true;
+});
 });
